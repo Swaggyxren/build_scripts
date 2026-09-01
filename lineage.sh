a8f3dedaec1a4a8e4265b5bb7a0e511a571b52b2
Swaggyxren <masambo67@gmail.com>
2026-09-01 13:41:26 +0800

    lineage.sh: add all device, kernel, vendor, mediatek & dolby dependencies

--- lineage.sh
+++ lineage.sh
@@ -5,13 +5,25 @@ set -e
 #  LineageOS Build Script for Tecno Pova 5 Pro 5G (LH8n)
 # ==============================================================================
 
-echo "=== [1/5] Cleaning existing device and vendor trees ==="
-rm -rf device/tecno/LH8n vendor/tecno/LH8n kernel/tecno/LH8n
+echo "=== [1/5] Cleaning previous trees ==="
+rm -rf device/tecno/LH8n \
+       vendor/tecno/LH8n \
+       device/tecno/LH8n-kernel \
+       vendor/mediatek/ims \
+       device/mediatek/sepolicy_vndr \
+       hardware/mediatek \
+       vendor/sony/dolby \
+       vendor/lineage-priv/keys
 
-echo "=== [2/5] Cloning Tecno LH8n Trees ==="
-git clone https://github.com/Swaggyxren/android_device_tecno_LH8n.git --depth 1 -b lineage-23.2 device/tecno/LH8n
-git clone https://github.com/Swaggyxren/vendor_tecno_LH8n.git --depth 1 -b lineage-23.2 vendor/tecno/LH8n
-git clone https://github.com/Swaggyxren/android_kernel_tecno_LH8n.git --depth 1 -b main kernel/tecno/LH8n
+echo "=== [2/5] Cloning Device, Vendor, Kernel & Dependencies ==="
+git clone https://github.com/Swaggyxren/android_device_tecno_LH8n.git --depth 1 -b los-test device/tecno/LH8n
+git clone https://github.com/Swaggyxren/vendor_tecno_LH8n.git --depth 1 -b vendor-test vendor/tecno/LH8n
+git clone https://github.com/Swaggyxren/android_kernel_tecno_LH8n.git --depth 1 -b test device/tecno/LH8n-kernel
+git clone https://github.com/techyminati/android_vendor_mediatek_ims --depth 1 vendor/mediatek/ims
+git clone https://github.com/crdroidandroid/android_device_mediatek_sepolicy_vndr --depth 1 device/mediatek/sepolicy_vndr
+git clone https://github.com/crdroidandroid/android_hardware_mediatek.git --depth 1 -b 16.0 hardware/mediatek
+git clone https://github.com/naden01/android_vendor_sony_dolby --depth 1 vendor/sony/dolby
+git clone https://gitlab.com/naden01/keys-los23.git --depth 1 vendor/lineage-priv/keys
 
 echo "=== [3/5] Resyncing Sources ==="
 /opt/crave/resync.sh
